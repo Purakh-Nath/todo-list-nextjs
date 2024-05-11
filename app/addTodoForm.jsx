9fa9944330a87aaa26ac8e1246779025a09e7207
Purakh-Nath <purakhnath45@gmail.com>
2024-05-11 15:48:36 +0530

    Form Color Update

--- app/addTodoForm.jsx
+++ app/addTodoForm.jsx
@@ -45,8 +45,8 @@ const AddTodoForm = () => {
   return (
     <form onSubmit={submitHandler}>
     <div className="flex flex-col items-center justify-center py-8 space-y-4">
-  <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Todo Title" className=" bg-slate-300 w-full rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50 font-bold" />
-  <textarea value={description} onChange={(e) => setDescription(e.target.value)} type="text"  rows="5" placeholder="Todo Description" className="bg-slate-400 w-full rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50 font-bold"></textarea>
+  <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Todo Title" className=" bg-slate-800 text-white w-full rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50 font-bold" />
+  <textarea value={description} onChange={(e) => setDescription(e.target.value)} type="text"  rows="5" placeholder="Todo Description" className="bg-slate-900 text-white w-full rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50 font-bold"></textarea>
   <button type="submit" className="text-white  end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Todo</button>
 </div>
 </form>
